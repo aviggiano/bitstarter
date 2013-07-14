957251e1b604428fa0cd24df3f25392bcc36c2b2
Antonio Viggiano <antonio.viggiano@usp.br>
2013-07-14 06:14:21 +0000

    not working

--- grader.js
+++ grader.js
@@ -61,11 +61,30 @@ var clone = function(fn) {
         return fn.bind({});
     };
 
+var getFromUrl = function(url) {
+    var rest = require('restler');
+    var sys = require('util');  
+
+    rest.get(url).on('complete', function(result) {
+	  if (result instanceof Error) {
+	          sys.puts('Error: ' + result.message);
+	          this.retry(5000); // try again after 5 sec
+	        } else {
+		        fs.writeFileSync("dl-index.html", result);
+		      }
+	});
+}
+
 if(require.main == module) {
         program
             .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
             .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	    .option('-u, --url <url>', 'Path to url')
             .parse(process.argv);
+if (program.url != null) {
+getFromUrl(program.url);
+program.file = "dl-index.html";
+}
         var checkJson = checkHtmlFile(program.file, program.checks);
         var outJson = JSON.stringify(checkJson, null, 4);
         console.log(outJson);
